f404bf238cb60585cfbe00774abaa54af200a083
tin.keita <ipb23.t.paegle@vtdt.edu.lv>
2025-04-03 09:08:57 +0300

    kaut kas izdarits gn

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\ToDo;
+use App\Models\Diary;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,10 +16,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        ToDo::factory()->count(10)->create();
+        Diary::factory()->count(10)->create();
     }
 }
